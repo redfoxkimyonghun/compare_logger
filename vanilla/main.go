b26b0e83c35210f11c6bd89fb6660bfb7acda35e
redfoxkimyonghun <kim.yonghun@redfox.co.jp>
2015-05-27 15:01:21 +0900

    add sleep

--- vanilla/main.go
+++ vanilla/main.go
@@ -1,8 +1,24 @@
 package main
 import "fmt"
+import "time"
+
+const LOOP = 5
+
 func main() {
-	go func() {
-		fmt.Println("another goroutine")
-	}()
-	fmt.Println("main goroutine")
+	for g:=0; g<5; g++ {
+		go vanilla(g)
+	}
+	for s:=0; s<25; s++ {
+		fmt.Println(fmt.Sprintf("main sleep(%d)",s))
+		time.Sleep(time.Second)
+	}
+}
+func vanilla(a int) {
+	for i:=0; i<LOOP; i++ {
+		fmt.Println(fmt.Sprintf("(%d)goroutine : step %d", a, i))
+		for s:=0; s<a; s++ {
+			time.Sleep(time.Second)
+		}
+	}
+	fmt.Println(fmt.Sprintf("(%d)goroutine : finished", a))
 }
