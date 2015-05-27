4d123e20bba001f2dfec6ca1da8774345c535f6e
redfoxkimyonghun <kim.yonghun@redfox.co.jp>
2015-05-28 08:22:19 +0900

    ログレベル分ける

--- vanilla/main.go
+++ vanilla/main.go
@@ -9,16 +9,29 @@ func main() {
 		go vanilla(g)
 	}
 	for s:=0; s<LOOP * LOOP; s++ {
-		fmt.Println(fmt.Sprintf("main sleep(%d)",s))
+		log_warn(fmt.Sprintf("main sleep(%d)",s))
 		time.Sleep(time.Second)
 	}
 }
 func vanilla(a int) {
 	for i:=0; i<LOOP; i++ {
-		fmt.Println(fmt.Sprintf("(%d)goroutine : step %d", a, i))
+		log_info(fmt.Sprintf("(id %d)goroutine : step %d", a, i))
 		for s:=0; s<a; s++ {
 			time.Sleep(time.Second)
 		}
 	}
-	fmt.Println(fmt.Sprintf("(%d)goroutine : finished", a))
+	log_warn(fmt.Sprintf("(id %d)goroutine : finished", a))
 }
+func log_info(s string) {
+	s="[INFO]" + s
+	fmt.Println(s)
+}
+func log_warn(s string) {
+	s="[WARN]" + s
+	fmt.Println(s)
+}
+func log_error(s string) {
+	s="[ERROR]" + s
+	fmt.Println(s)
+}
+
