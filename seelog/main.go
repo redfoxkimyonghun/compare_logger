409ea0d00be278d13ec2000d21ae55dbbf44cda3
redfoxkimyonghun <kim.yonghun@redfox.co.jp>
2015-05-27 15:15:23 +0900

    とりあえずstdioに出す

--- seelog/main.go
+++ seelog/main.go
@@ -1,24 +1,52 @@
 package main
 import "fmt"
 import "time"
+import log "github.com/cihub/seelog"
 
 const LOOP = 15
 
+func configLogger() {
+    config := `
+<seelog type="sync">
+    <outputs>
+        <filter levels="trace,debug,info">
+            <console formatid="ltsv"/>
+        </filter>
+        <filter levels="warn,error,critical">
+            <console formatid="ltsv_error"/>
+        </filter>
+        <file formatid="ltsv" path="result.log"/>
+    </outputs>
+    <formats>
+        <format id="ltsv" format="time:%Date(2006-01-02T15:04:05.000Z07:00)%tlev:%l%tmsg:%Msg%n"/>
+        <format id="ltsv_error"
+            format="%EscM(31)time:%Date(2006-01-02T15:04:05.000Z07:00)%tlev:%l%tmsg:%Msg%EscM(0)%n"/>
+    </formats>
+</seelog>`
+
+    logger, err := log.LoggerFromConfigAsBytes([]byte(config))
+    if err != nil {
+        panic(err)
+    }
+    log.ReplaceLogger(logger)
+}
+
 func main() {
+	configLogger()
 	for g:=0; g<LOOP; g++ {
 		go vanilla(g)
 	}
 	for s:=0; s<LOOP * LOOP; s++ {
-		fmt.Println(fmt.Sprintf("main sleep(%d)",s))
+		log.Info(fmt.Sprintf("main sleep(%d)",s))
 		time.Sleep(time.Second)
 	}
 }
 func vanilla(a int) {
 	for i:=0; i<LOOP; i++ {
-		fmt.Println(fmt.Sprintf("(%d)goroutine : step %d", a, i))
+		log.Info(fmt.Sprintf("(%d)goroutine : step %d", a, i))
 		for s:=0; s<a; s++ {
 			time.Sleep(time.Second)
 		}
 	}
-	fmt.Println(fmt.Sprintf("(%d)goroutine : finished", a))
+	log.Info(fmt.Sprintf("(%d)goroutine : finished", a))
 }
