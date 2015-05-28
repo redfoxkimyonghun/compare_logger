b8e45573c21253bc6a44ce3ecd4a21e949a03600
redfoxkimyonghun <kim.yonghun@redfox.co.jp>
2015-05-28 13:08:23 +0900

    merge from vanilla update

--- logxi/main.go
+++ logxi/main.go
@@ -9,16 +9,33 @@ func main() {
 		go vanilla(g)
 	}
 	for s:=0; s<LOOP * LOOP; s++ {
-		fmt.Println(fmt.Sprintf("main sleep(%d)",s))
+		log_info(fmt.Sprintf("main sleep(%d)",s))
 		time.Sleep(time.Second)
 	}
 }
 func vanilla(a int) {
+	log_info(fmt.Sprintf("(id %d)goroutine : start", a))
 	for i:=0; i<LOOP; i++ {
-		fmt.Println(fmt.Sprintf("(%d)goroutine : step %d", a, i))
+		log_debug(fmt.Sprintf("(id %d)goroutine : step %d", a, i))
 		for s:=0; s<a; s++ {
 			time.Sleep(time.Second)
 		}
+		if i % 3 == 0 {
+			log_warn(fmt.Sprintf("(id %d)goroutine : some trouble occurred : step %d", a, i))
+		}
 	}
-	fmt.Println(fmt.Sprintf("(%d)goroutine : finished", a))
+	log_info(fmt.Sprintf("(id %d)goroutine : finished", a))
 }
+func log_debug(s string) {
+	fmt.Println("[DEBUG]"+s)
+}
+func log_info(s string) {
+	fmt.Println("[INFO]"+s)
+}
+func log_warn(s string) {
+	fmt.Println("[WARN]"+s)
+}
+func log_error(s string) {
+	fmt.Println("[ERROR]"+s)
+}
+
